5f466286d3660fdd0cd8ea7db35ccd21e3e662c0
hisamuddin-coder <mdhisamuddin1993@gmail.com>
2024-08-02 23:39:24 +0530

    added hero section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,13 @@
 "use strict";
-console.log("hello world");
-``;
+//////////////// SELECTORS /////////////////////
+const navToggle = document.querySelector(".nav-toggle");
+const navContainer = document.querySelector(".nav-links-container");
+const navLinks = document.querySelector(".nav-links");
+
+navToggle.addEventListener("click", () => {
+  const linksHeight = navLinks.getBoundingClientRect().height;
+  const navContainerHeight = navContainer.getBoundingClientRect().height;
+
+  navContainer.style.height = navContainerHeight === 0 ? `${linksHeight}px` : 0;
+  navToggle.classList.toggle("active", navContainerHeight === 0);
+});
